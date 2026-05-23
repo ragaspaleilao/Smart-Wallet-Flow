1d5d591d92dc55df1bd6ba552b887fef46ca3800
ragaspaleilao <ragaspa@gmail.com>
2026-05-23 14:40:31 -0300

    Fix: bind host 0.0.0.0 para Railway conseguir rotear o tráfego

--- server/index.ts
+++ server/index.ts
@@ -100,7 +100,7 @@ app.use((req, res, next) => {
   httpServer.listen(
     {
       port,
-      host: "localhost",
+      host: "0.0.0.0",
     },
     () => {
       log(`serving on port ${port}`);
